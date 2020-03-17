f80d703fb4cd82c224263b7f03515937f9216fa2
s18938 <s18938@pjwstk.edu.pl>
2020-03-17 14:18:12 +0100

    new FileChanel.open

--- src/zad1/Futil.java
+++ src/zad1/Futil.java
@@ -4,18 +4,14 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
 public class Futil {
 
-    public Futil() {
-    }
-
-
     public static void processDir(String startDirName, String resultFileName) {
         try {
+            FileChannel.open(Paths.get(resultFileName), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE).write(ByteBuffer.allocate(0));
             Files.walkFileTree(Paths.get(startDirName), new FileVisitor<Path>() {
                 @Override
                 public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
@@ -24,15 +20,14 @@ public class Futil {
 
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    FileChannel chanelReader = FileChannel.open(file);
+                    FileChannel chanelReader = FileChannel.open(file, StandardOpenOption.READ);
                     ByteBuffer buf = ByteBuffer.allocate((int) chanelReader.size());
                     chanelReader.read(buf);
                     chanelReader.close();
                     buf.flip();
-                    FileChannel chanelWriter = FileChannel.open(Paths.get(resultFileName),StandardOpenOption.CREATE, StandardOpenOption.WRITE);// new RandomAccessFile(resultFile, "rw").getChannel();
-                    chanelWriter.write(ByteBuffer.wrap(new String(buf.array(), Charset.forName("Cp1250")).getBytes(StandardCharsets.UTF_8)), chanelWriter.size());
+                    FileChannel chanelWriter = FileChannel.open(Paths.get(resultFileName), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+                    chanelWriter.write(Charset.forName("UTF-8").encode(Charset.forName("Cp1250").decode(buf)), chanelWriter.size());
                     chanelWriter.close();
-
                     return FileVisitResult.CONTINUE;
                 }
 
@@ -46,7 +41,6 @@ public class Futil {
                     return FileVisitResult.CONTINUE;
                 }
             });
-
         } catch (IOException e) {
             e.printStackTrace();
         }
